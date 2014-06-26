970ad322f4f17b1f7be6f8d6fa129c3d472516f9
Daniel Hanf <danhanf@gmail.com>
2014-06-26 17:11:22 -0400

    app

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express')
+  , http = require('http')
+  , routes = require('./routes')
+  , _ = require('lodash')
+
+
+var app = express();
+
+// config app
+app.set('port', process.env.PORT || 80);
+app.set('views', __dirname+'/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', home);
+
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log("express server listening on port "+port)
+});
