415bb640eb03ad61e8a08f084d73cc7b61262363
Daniel Hanf <danhanf@gmail.com>
2014-08-18 16:37:19 -0400

    it kind of works now

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,45 @@
 var fs = require('fs')
   , _ = require('lodash')
+  , path = require('path')
+  , marked = require('marked')
 
 exports.index = function(req, res) {
-  fs.readdir(__dirname+'/../../posts/improv/', function(err, improv) {
+  getImprovContent(function(err, improv) {
+    getPostContent(function(err, post) {
+      getComedyContent(function(err, comedy) {
+        res.render('index', {title: 'welcome to dan', improv:improv, post:post, comedy:comedy})
+      })
+    })
+  })
+}
+
+function getImprovContent(cb) {
+  fs.readdir(__dirname+'/../posts/improv', function(err, improv) {
     var recentImprov = improv[improv.length-1]
     fs.readFile(__dirname+'/../posts/improv/'+recentImprov, 'utf8', function(err, content) {
       var improvContent = marked(content)
+      console.log(improvContent)
+      cb(err, improvContent)
     })
   })
-  fs.readdir(__dirname+'/../posts/posts/', function(err, posts) {
-    var recentImprov = posts[posts.length-1]
-    fs.readFile(__dirname+'/../../posts/posts/'+recentPosts, function(err, content) {
-      var postsContent = marked(content)
+}
+
+function getPostContent(cb) {
+  fs.readdir(__dirname+'/../posts/posts', function(err, post) {
+    var recentPost = post[post.length-1]
+    fs.readFile(__dirname+'/../posts/posts/'+recentPost, 'utf8', function(err, content) {
+      var postContent = marked(content)
+      cb(err, postContent)
     })
   })
+}
+
+function getComedyContent(cb) {
   fs.readdir(__dirname+'/../posts/comedy/', function(err, comedy) {
     var recentComedy = comedy[comedy.length-1]
-    fs.readFile(__dirname+'/../posts/comedy'+recentComedy, function(err, content) {
+    fs.readFile(__dirname+'/../posts/comedy/'+recentComedy, 'utf8', function(err, content) {
       var comedyContent = marked(content)
+      cb(err, comedyContent)
     })
   })
-  res.render('index', {title: 'welcome to dan', improv:improvContent, posts:postsContent, comedy:comedyContent})
-}
+}  
